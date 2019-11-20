9bff5915459fe706a16fff537f62b9048f657b78
= <=>
2019-11-19 21:47:10 -0300

    Modificaçoes em nodejs, criando a api

--- web_app_test/app.js
+++ web_app_test/app.js
@@ -15,22 +15,21 @@ const server = http.createServer((req, res) => {
     var python_code;
     var sample = url_split[2]; //resolução do gráfico gerado
     var title = url_split[3]; //titulo do gráfico gerado
-    // var title_rewrite = decodeURI(title);
+    //var title_rewrite = decodeURI(title);
     exec('python3 ../middleware.py ' + sample + '', (err, stdout, stderr) => {
       if (err) {
         return;
       }
       python_code = JSON.parse(stdout);
-      var python_code_text =  JSON.stringify(python_code);
+      var python_code_text = JSON.stringify(python_code);
+      res.write('<img src="data:image/png;base64,' + python_code.data_img + '">');
       res.end(python_code_text);
     });
   }
   app.get('/user/:id/', function (req, res) {
     res.send('user' + req.params.id);
   });
-
 });
-
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
