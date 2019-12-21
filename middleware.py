6e898777e8327e8635b4076e7373757dd07f35a6
Paulo Henrique <phvcandido@gmail.com>
2019-12-21 17:58:00 -0200

    modificaçoes na api

--- middleware.py
+++ middleware.py
@@ -9,7 +9,9 @@ import sys
 def tratar_entradas():
     resolucao = int(1)
     titulo = str("Intensidade do campo Elétrico")
-   
+    wl = 1064E-9
+    axicon = 2 * npx.pi / 180
+
     if  len(sys.argv) == 1 :
         return false
     else:
@@ -17,17 +19,22 @@ def tratar_entradas():
             resolucao = sys.argv[1]
             
         if(len(sys.argv) > 2):
-            titulo = sys.argv[2]
+            titulo = sys.argv[2] 
             
         if(len(sys.argv) > 3):
-            wl = sys.argv[3]
+            wl = float(sys.argv[3]) #comprimento da onda
+        
+        if(len(sys.argv) > 4):
+            axicon = float(sys.argv[4]) #comprimento da onda
+        
    
-    gerar_visualizacao_intensidade_campo_eletrico(resolucao = resolucao, titulo = titulo)
+    gerar_visualizacao_intensidade_campo_eletrico(wl=wl, resolucao = resolucao, titulo = titulo, axicon = axicon)
 
-def gerar_visualizacao_intensidade_campo_eletrico(wl = 1064E-9, resolucao = 1, titulo="Intensidade do campo Elétrico"):
+def gerar_visualizacao_intensidade_campo_eletrico(wl = 1064E-9, resolucao = 1, titulo="Intensidade do campo Elétrico", axicon = 2 * npx.pi / 180 ):
     k = 2 * npx.pi / wl          # número de onda
     axicon = 2 * npx.pi / 180    # ângulo de axicon (\alpha)
     tmbscs = {}                 # fatores de forma (modo TM) [BSCs]
+    
     #criando fatores de forma
     for n in range(1, 1000):
         tmbscs[(n, 1)] = bsc.bessel_bsc_tm(n, 1, axicon, k=k)
@@ -39,6 +46,7 @@ def gerar_visualizacao_intensidade_campo_eletrico(wl = 1064E-9, resolucao = 1, t
     bscs = {'TM': tmbscs, 'TE': tebscs} # Todos os BSCs devem estar neste formato
 
     pw = fld.SphericalElectricField(k, bscs=bscs)   # objeto que descreve o campo elétrico
+    
     v1 = pw.norm(1E-6, npx.pi / 4, 0)    # um valor pra norma do campo elétrico em um ponto do espaço
     print(pw.plot_r(2E-5, sample=resolucao, title=titulo)) # Plotagem da intensidade (ao quadrado) campo elétrico num corte do plano-xz
 
--- web_app_test/app.js
+++ web_app_test/app.js
@@ -1,3 +1,4 @@
+
 const http = require('http');
 var express = require('express');
 var fs = require('fs');
@@ -6,57 +7,40 @@ const port = 3000;
 var app = express();
 var { exec } = require('child_process');
 
-app.get("/", (req,res) => {
-	 res.setHeader('Content-Type', 'text/html');
-	res.send("Entre com um valor de resolução e título para execução");
-
+app.get("/", (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.send("Entre com um valor de resolução e título para execução");
 });
 
-app.use("/exec/:sample/:title", (req, res) => {
+app.use("/exec/:sample/:title/:wl/:axicon", (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   let sample = req.params.sample;
   let title = req.params.title;
+  let wl = req.params.wl;
+  let axicon = req.params.axicon;
   var python_code;
   var python_code_text;
-  var command = 'python3 ../middleware.py ' + sample + ' ' + title;
-  var path = '../tmp.json';
-
-  res.setHeader('Content-Type', 'text/html');
+  var command = 'python3 ../middleware.py ' + sample + ' ' + title + ' ' + wl + ' ' + axicon;
+  var path = '../tmp.json'; i
   exec(command, (err, stdout, stderr) => {
     if (err) {
       return;
     }
+
     python_code = JSON.parse(stdout);
     python_code_text = JSON.stringify(python_code);
-
-    res.write('<img src="data:image/png;base64,' + python_code.data_img + '">');
-    res.write(python_code_text);
+    res.end(python_code_text);
   });
-  // res.json([sample, title, command, python_code, python_code_text]);
+
 });
-
-// const server = http.createServer((req, res) => {
-
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/html');
-//   var url = req.url;
-//   if (url.match("exec/")) {
-//     var { exec } = require('child_process');
-//     var url_split = url.split("/"); //quebrando url
-//     var python_code;
-//     var sample = url_split[2]; //resolução do gráfico gerado
-//     var title = url_split[3]; //titulo do gráfico gerado
-//     //var title_rewrite = decodeURI(title);
-//     exec('python3 ../middleware.py ' + sample + '', (err, stdout, stderr) => {
-//       if (err) {
-//         return;
-//       }
-//       python_code = JSON.parse(stdout);
-//       var python_code_text = JSON.stringify(python_code);
-//       res.write('<img src="data:image/png;base64,' + python_code.data_img + '">');
-//       res.end(python_code_text);
-//     });
-//   }
-// });
 app.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
